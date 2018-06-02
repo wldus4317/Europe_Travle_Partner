2631e20eb1fe59bd9d5474347c21695726ca8364
wldus4317 <31752806+wldus4317@users.noreply.github.com>
2018-06-02 20:27:50 +0900

    Update member_form.php

--- php/login/member_form.php
+++ php/login/member_form.php
@@ -1,7 +1,7 @@
 <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
+
 
 <div class="container">
             <form class="form-horizontal" role="form">
@@ -10,6 +10,12 @@
                     <label for="firstName" class="col-sm-3 control-label">이름</label>
                     <div class="col-sm-6">
                         <input type="text" id="firstName" placeholder="이름을 입력하세요" class="form-control" autofocus>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="id" class="col-sm-3 control-label">아이디</label>
+                    <div class="col-sm-6">
+                        <input type="text" id="id" placeholder="아이디를 입력하세요" class="form-control">
                     </div>
                 </div>
                 <div class="form-group">
